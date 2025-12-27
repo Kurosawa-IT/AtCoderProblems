e266b92a1ec4782353159e0280722dff0d3291a0
Kurosawa-IT <kurosawaitbusiness@gmail.com>
2025-12-28 02:54:34 +0900

    A問題

---
+++ 431/a.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int h, b;
+    cin >> h >> b;
+
+    if (h > b)
+    {
+        cout << h - b << endl;
+    }
+    else
+    {
+        cout << 0 << endl;
+    }
+}
