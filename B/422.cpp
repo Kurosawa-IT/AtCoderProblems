f3ca80c2ced32bfadd21986a4d0dbd18338a2e05
Kurosawa-IT <kurosawaitbusiness@gmail.com>
2026-01-03 18:54:54 +0900

    B422

--- B/422.cpp
+++ B/422.cpp
@@ -5,4 +5,52 @@ int main()
 {
     int h, w;
     cin >> h >> w;
+
+    vector<vector<char>> s(h + 2, vector<char>(w + 2));
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            cin >> s.at(i).at(j);
+        }
+    }
+
+    bool flag = true;
+
+    for (int i = 1; i <= h; i++)
+    {
+        for (int j = 1; j <= w; j++)
+        {
+            int cnt = 0;
+            if (s.at(i - 1).at(j) == '#')
+            {
+                cnt++;
+            }
+            if (s.at(i + 1).at(j) == '#')
+            {
+                cnt++;
+            }
+            if (s.at(i).at(j - 1) == '#')
+            {
+                cnt++;
+            }
+            if (s.at(i).at(j + 1) == '#')
+            {
+                cnt++;
+            }
+            if (s.at(i).at(j) == '#' && !(cnt == 2 || cnt == 4))
+            {
+                flag = false;
+            }
+        }
+    }
+
+    if (flag)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
 }
